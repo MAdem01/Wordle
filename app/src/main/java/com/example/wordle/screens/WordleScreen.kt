831fe65759a1877808950e3a82873251ef861d0b
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-04 16:00:06 +0200

    add CenterAlignedTopAppBar

--- app/src/main/java/com/example/wordle/screens/WordleScreen.kt
+++ app/src/main/java/com/example/wordle/screens/WordleScreen.kt
@@ -1,15 +1,37 @@
 package com.example.wordle.screens
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun WordleScreen(navController: NavController){
-    Scaffold() { paddingValues ->
+fun WordleScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Black),
+                title = {
+                    Text(
+                        text = "Wordle",
+                        color = Color.White,
+                        fontStyle = FontStyle.Italic,
+                        fontSize = 35.sp
+                    )
+                }
+            )
+        }
+    ) { paddingValues ->
         Surface(modifier = Modifier.padding(paddingValues)) {
         }
     }
