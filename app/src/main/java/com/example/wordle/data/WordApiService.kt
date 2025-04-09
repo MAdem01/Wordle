89d6b8ce95e0ff972b2d11a5aac0dae0663c7292
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-09 12:47:54 +0200

    add retrofit interface and client

---
+++ app/src/main/java/com/example/wordle/data/WordApiService.kt
@@ -0,0 +1,10 @@
+package com.example.wordle.data
+
+import android.health.connect.datatypes.units.Length
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WordApiService {
+    @GET("word")
+    suspend fun getRandomWord(@Query("length") length: Int = 5): List<String>
+}
